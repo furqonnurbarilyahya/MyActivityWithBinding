d84ad9833df18070bbe1649479f487d7898c0a10
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-09-20 21:54:18 +0700

    first commit

---
+++ app/src/main/java/com/bangkit/belajar/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.bangkit.belajar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.bangkit.belajar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            binding.tvResult.text = result
+        }
+
+    }
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_calculate) {
+            val inputLength = binding.edtLength.text.toString().trim()
+            val inputWidth = binding.edtWidth.text.toString().trim()
+            val inputHeight = binding.edtHeight.text.toString().trim()
+            var isEmptyFields = false
+
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                binding.edtLength.error = "Field tidak boleh kosong"
+            }
+
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                binding.edtWidth.error = "Field tidak boleh kosong"
+            }
+
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                binding.edtHeight.error = "Field tidak boleh kosong"
+            }
+
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                binding.tvResult.text = volume.toString()
+            }
+        }
+    }
+}
